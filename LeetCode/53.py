e4a167d82e92efdfb16797a1a69cfef2ca7fb3a9
Injun Hwang <injun.hwang@travelflan.com>
2020-09-14 23:50:26 +0900

    53

---
+++ LeetCode/53.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def maxSubArray(nums: List[int]) -> int:
+    memoi_list = [-9999999 for i in range(len(nums))]
+    memoi_list[0] = nums[0]
+    
+    for i in range(1, len(nums)):
+        if memoi_list[i-1] + nums[i] > nums[i]:
+            memoi_list[i] = memoi_list[i-1] + nums[i]
+        else:
+            memoi_list[i] = nums[i]
+    print(memoi_list)
+    return max(memoi_list)
+
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
