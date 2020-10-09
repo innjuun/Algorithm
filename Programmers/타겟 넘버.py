ed018a05f078e543852bdbd1557fda73d05b5e18
innjuun <sroo2315@gmail.com>
2020-10-10 00:57:28 +0900

    Better solution not using global variable

--- Programmers/타겟 넘버.py
+++ Programmers/타겟 넘버.py
@@ -1,18 +1,35 @@
-cnt = 0
+# cnt = 0
 
 
+# def solution(numbers, target):
+#     dfs(0, numbers, target, 0)
+#     return cnt
+
+# def dfs(index, numbers, target, sum):
+#     global cnt
+#     if index == len(numbers):
+#         if sum == target:
+#             cnt += 1
+#         return
+
+#     dfs(index+1, numbers, target, sum + numbers[index])
+#     dfs(index+1, numbers, target, sum - numbers[index])
+
+# solution([1, 1, 1, 1, 1], 3)
+
+
+# better solution, not using global variable
 def solution(numbers, target):
-    dfs(0, numbers, target, 0)
-    return cnt
+    answer = dfs(0, numbers, target, 0)
+    return answer
 
 def dfs(index, numbers, target, sum):
-    global cnt
     if index == len(numbers):
         if sum == target:
-            cnt += 1
-        return
+            return 1
+        else:
+            return 0
 
-    dfs(index+1, numbers, target, sum + numbers[index])
-    dfs(index+1, numbers, target, sum - numbers[index])
+    return dfs(index+1, numbers, target, sum + numbers[index]) + dfs(index+1, numbers, target, sum - numbers[index])
 
 solution([1, 1, 1, 1, 1], 3)
