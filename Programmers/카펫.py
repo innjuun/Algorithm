e7451bce91dce6c7c8123f0c87f1db850f0d8da3
innjuun <sroo2315@gmail.com>
2020-10-12 00:18:09 +0900

    카펫

---
+++ Programmers/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    for y in range(3, brown // 2):
+        x = 3
+        while x < brown //2:
+            if (x + y) * 2 - 4 == brown and y >= x and (x-2) * (y-2) == yellow:
+                return [y, x]
+            x += 1
