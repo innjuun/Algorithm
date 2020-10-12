b074aefe148c999c24ea4821d838fa38cc48d042
innjuun <sroo2315@gmail.com>
2020-10-13 01:41:20 +0900

    체육복, 다리를 지나는 트럭, 더 맵게

---
+++ Programmers/다리를 지나는 트럭.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    truck_weights = deque(truck_weights)
+    crossing = deque()
+    answer = 1
+    while truck_weights or crossing:
+
+        total_weight = sum(i for i, j in crossing)
+        if truck_weights and weight >= (total_weight + truck_weights[0]):
+            crossing.append([truck_weights.popleft(), bridge_length])
+        for i in range(len(crossing)):
+            crossing[i][1] -= 1
+        answer += 1
+        while crossing and crossing[0][1] == 0:
+            crossing.popleft()
+    return answer
+
+solution(1, 10, [6])
---
+++ Programmers/더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    answer = 0
+    while len(scoville) >= 2:
+        hot = heapq.heappop(scoville)
+        second_hot = heapq.heappop(scoville)
+        if hot >= K:
+            return answer
+        elif hot + (second_hot * 2) >= K:
+            return answer + 1
+        heapq.heappush(scoville, hot + (second_hot * 2))
+        answer += 1
+    if heapq.heappop(scoville) >= K:
+        return answer
+    return -1
