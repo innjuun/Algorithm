662e3bc8dc36f0a164486d37a0fa7a666a1ca717
innjuun <sroo2315@gmail.com>
2020-10-14 00:40:43 +0900

    큰수만들기

---
+++ Programmers/큰 수 만들기.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    stack = []
+    tmp = k
+    for i in range(len(number)):
+        stack.append(number[i])
+        while tmp >0 and i < len(number) -1 and stack and stack[-1] < number[i + 1]:
+            stack.pop()
+            tmp -= 1
+    if tmp == k:
+        return number[:-k]
+    return ''.join(stack)
