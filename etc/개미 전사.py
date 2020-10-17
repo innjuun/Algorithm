a87410280820e70406e4c61ec7187cde1618caa9
innjuun <sroo2315@gmail.com>
2020-10-17 16:32:16 +0900

    개미 전사

---
+++ etc/개미 전사.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+K = list(map(int, input().split()))
+
+checked = [0] * N
+
+checked[0] = K[0]
+checked[1] = K[1]
+
+for i in range(2, N):
+    checked[i] = max(checked[i-2] + K[i], checked[i-1])
+
+print(checked)
