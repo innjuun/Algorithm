f171a02dfd69e37ebcaf73645b61911f96459716
innjuun <sroo2315@gmail.com>
2020-10-27 23:15:06 +0900

    198

---
+++ LeetCode/easy/198 house robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        memo = [0] * len(nums)
+        memo[0] = nums[0]
+        memo[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            memo[i] = max(memo[i-2] + nums[i], memo[i-1])
+        print(memo)
+        return max(memo)
