e27085a7fda36afc142872d69cfba2edc95aaabc
innjuun <sroo2315@gmail.com>
2020-10-29 23:30:41 +0900

    1470

---
+++ LeetCode/easy/1470 shuffle the array.py
@@ -0,0 +1,12 @@
+from collections import deque
+class Solution:
+    
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        q1 = deque(nums[:n])
+        q2 = deque(nums[n:])
+        li = []
+        for i in range(n):
+            li.append(q1.popleft())
+            li.append(q2.popleft())
+            
+        return li
