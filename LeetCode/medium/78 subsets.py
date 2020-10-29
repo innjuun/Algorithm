94bbebca2aa5baeb2466ff84757cfead05e8fccc
innjuun <sroo2315@gmail.com>
2020-10-30 00:37:44 +0900

    78 by dp

--- LeetCode/medium/78 subsets.py
+++ LeetCode/medium/78 subsets.py
@@ -6,3 +6,13 @@ class Solution:
             for c in combinations(nums, i):
                 answer.append(list(c))
         return answer
+    
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        answer = [[]]
+        
+        for i in range(len(nums)):
+            for j in range(len(answer)):
+                answer.append(answer[j] +[nums[i]])
+        
+        return answer
