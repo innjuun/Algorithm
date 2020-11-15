c69d6a2197f0ec20af7afb4113db0fd1141840b6
innjuun <sroo2315@gmail.com>
2020-11-15 16:42:41 +0900

    215

---
+++ LeetCode/medium/215 kth largest array.py
@@ -0,0 +1,22 @@
+
+#using sort
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[-k]
+
+
+# using heap
+from heapq import heappush, heappop
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        
+        for i in range(len(nums)):
+            heappush(heap, nums[i])
+            
+            if len(heap) > k:
+                heappop(heap)
+
+        return heappop(heap)
