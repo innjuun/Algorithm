fbc25bdf08b4f52a0e2d010366fb362981f731a1
innjuun <sroo2315@gmail.com>
2020-11-15 14:48:00 +0900

    49

---
+++ LeetCode/medium/49 group anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        dic = {}
+        for s in strs:
+            ordered_s = ''.join(sorted(s))
+            if ordered_s in dic:
+                dic[ordered_s] = dic[ordered_s] + [s]
+                print(dic[ordered_s])
+            else:
+                dic[ordered_s] = [s]
+        ans = []
+        for d in dic.values():
+            ans.append(d)
+        return ans
+
+s = Solution()
+s.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
