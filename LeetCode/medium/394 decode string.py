0ffd9bd9917efe6e74601888fbd55ddf80119f03
innjuun <sroo2315@gmail.com>
2020-11-16 00:11:44 +0900

    fix

--- LeetCode/medium/394 decode string.py
+++ LeetCode/medium/394 decode string.py
@@ -10,15 +10,15 @@ class Solution:
                 count = count * 10 + int(i)
                 
             elif i == "[":
-                stack.append((current_string, count ))
+                stack.append((current_string, count))
                 count = 0
                 current_string = ""
 
             elif i == "]":
                 print(stack)
-                string, count = stack.pop()
-                current_string = string + count * current_string
-                
+                prev_string, prev_count = stack.pop()
+                current_string = prev_string + prev_count * current_string
+    
                 
             else:
                 current_string += i
