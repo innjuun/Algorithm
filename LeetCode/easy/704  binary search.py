be7436336ee10b89d451ec5e311a6f1fa378f622
innjuun <sroo2315@gmail.com>
2020-11-16 23:57:07 +0900

    iterative

--- LeetCode/easy/704  binary search.py
+++ LeetCode/easy/704  binary search.py
@@ -1,3 +1,5 @@
+# recursive
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
@@ -20,3 +22,21 @@ class Solution:
             return answer.pop()
         else:
             return -1
+        
+# iterative
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return -1
