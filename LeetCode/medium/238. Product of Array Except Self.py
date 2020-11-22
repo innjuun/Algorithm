6261e6defd5feeba17c3d0e7469e7b3cc04f7c25
innjuun <sroo2315@gmail.com>
2020-11-22 15:51:58 +0900

    238 with constant space complexity

--- LeetCode/medium/238. Product of Array Except Self.py
+++ LeetCode/medium/238. Product of Array Except Self.py
@@ -17,3 +17,19 @@ class Solution:
         ret[0] = backward[1]
         ret[len(nums)-1] = forward[len(nums)-2]
         return ret
+    
+# Follow up:
+# Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        forward = [1] * len(nums)
+        forward[0] = nums[0]
+        for i in range(1, len(nums)):
+            forward[i] = forward[i-1] * nums[i]
+        
+        r = 1
+        for i in range(len(nums)-1, 0 , -1):
+            forward[i] = forward[i-1] * r
+            r = r * nums[i]
+        forward[0] = r
+        return forward
