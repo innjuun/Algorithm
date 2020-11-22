d6ade69c3ddc46ed93e21edec6143fec49cdef94
innjuun <sroo2315@gmail.com>
2020-11-22 17:12:28 +0900

    11 two pointer

---
+++ LeetCode/medium/11. Container With Most Water.py
@@ -0,0 +1,31 @@
+# brute force, time limit exceeded
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        dp = [0] * len(height)
+        maximum = 0
+        for i in range(1, len(height)):
+            for j in range(0, i):
+                
+                maximum = max(maximum, min(height[i], height[j]) * (i-j))
+        return maximum
+    
+# two pointer solution
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        start = 0
+        end = len(height) - 1
+        
+        maximum = 0
+        while start <= end:
+            maximum = max(maximum, (end - start) * min(height[start], height[end]))
+            
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end -= 1
+        
+        return maximum
+            
+            
