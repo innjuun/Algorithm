a3891e879cf112dbc3ab240b1454964c274a56d2
innjuun <sroo2315@gmail.com>
2020-11-27 23:29:25 +0900

    114

---
+++ LeetCode/medium/114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,28 @@
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        stack = []
+        
+        stack.append(root)
+        
+        while stack:
+            current_node = stack.pop()
+            
+            if current_node.right:
+                stack.append(current_node.right)
+            if current_node.left:
+                stack.append(current_node.left)
+            if stack:
+                current_node.right = stack[-1]
+            current_node.left = None
