ac0cecd11e8f0854f29877407205f38028123623
innjuun <sroo2315@gmail.com>
2020-11-28 00:28:13 +0900

    279 accepted

--- LeetCode/medium/279. Perfect Squares.py
+++ LeetCode/medium/279. Perfect Squares.py
@@ -16,3 +16,18 @@ class Solution:
                 j += 1
         # print(memo)
         return memo[n]
+
+# accepted 
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo = [i for i in range(n+1)]
+        i = 1
+
+        
+        for i in range(1, n + 1):
+                    
+            memo[i] = min([memo[i-(j**2)] for j in range(1, int(i**0.5)+1)]) + 1
+
+        # print(memo)
+        return memo[n]
