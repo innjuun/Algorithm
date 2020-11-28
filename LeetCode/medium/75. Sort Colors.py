e662fb540af23fe12db87030738248780f06dd8a
innjuun <sroo2315@gmail.com>
2020-11-28 16:49:18 +0900

    75

---
+++ LeetCode/medium/75. Sort Colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        
+        start = 0
+        end = len(nums) - 1
+        index = 0
+        
+        while index <= end and start < end:
+            if nums[index] == 0:
+                nums[start], nums[index] = nums[index], nums[start]
+                start += 1
+                index += 1
+            elif nums[index] == 2:
+                nums[end], nums[index] = nums[index], nums[end]
+                end -= 1
+            
+            else:
+                index += 1
