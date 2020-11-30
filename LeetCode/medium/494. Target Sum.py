fb927964ac10c70d31cd1b48b737bb15b55c941c
innjuun <sroo2315@gmail.com>
2020-12-01 00:49:26 +0900

    494 dp, too hard TT

--- LeetCode/medium/494. Target Sum.py
+++ LeetCode/medium/494. Target Sum.py
@@ -1,21 +1,65 @@
-class Solution:
-    def findTargetSumWays(self, nums: List[int], S: int) -> int:
-        stack = []
-        count = 0
-        count = []
-        def dfs(index):
-            if index == len(nums):
-                # print(stack)
-                if sum(stack) == S:
-                    count.append(1)
-                return
+# # dfs, TLE
+
+# class Solution:
+#     def findTargetSumWays(self, nums: List[int], S: int) -> int:
+#         stack = []
+#         count = 0
+#         count = []
+#         def dfs(index):
+#             if index == len(nums):
+#                 # print(stack)
+#                 if sum(stack) == S:
+#                     count.append(1)
+#                 return
             
-            stack.append(nums[index])
-            dfs(index + 1)
-            stack.pop()
-            stack.append(-nums[index])
-            dfs(index + 1)
-            stack.pop()
+#             stack.append(nums[index])
+#             dfs(index + 1)
+#             stack.pop()
+#             stack.append(-nums[index])
+#             dfs(index + 1)
+#             stack.pop()
         
-        dfs(0)
-        return sum(count)
+#         dfs(0)
+#         return sum(count)
+    
+# # TLE
+# class Solution:
+#     count = 0
+#     def findTargetSumWays(self, nums: List[int], S: int) -> int:
+#         stack = []
+#         summary = 0
+#         def dfs(index, summary):
+#             if index == len(nums):
+#                 if summary == S:
+#                     self.count += 1
+#                 return
+            
+#             dfs(index + 1, summary + nums[index])
+            
+            
+#             dfs(index + 1, summary - nums[index])
+        
+#         dfs(0, summary)
+#         return self.count
+
+class Solution:
+    memo = {}
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        summary = 0
+        self.memo = {}
+        def dfs(index, summary):
+            if (index, summary) in self.memo:
+                return self.memo[(index, summary)]
+            if index == len(nums):
+                if summary == S:
+                    return 1
+                return 0
+            
+            positive = dfs(index + 1, summary + nums[index])
+            negative = dfs(index + 1, summary - nums[index])
+            
+            self.memo[(index, summary)] = positive + negative
+            return self.memo[(index, summary)]
+        answer = dfs(0, summary)
+
+        return answer
