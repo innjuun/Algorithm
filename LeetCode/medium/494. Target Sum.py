1d497b36cdf70f38e673950d1a9e63ad198c7f50
innjuun <sroo2315@gmail.com>
2020-12-03 02:02:57 +0900

    494 2D dp

--- LeetCode/medium/494. Target Sum.py
+++ LeetCode/medium/494. Target Sum.py
@@ -44,7 +44,7 @@
 
 class Solution:
     memo = {}
-    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+    def findTargetSumWays(self, nums, S: int) -> int:
         summary = 0
         self.memo = {}
         def dfs(index, summary):
@@ -63,3 +63,23 @@ class Solution:
         answer = dfs(0, summary)
 
         return answer
+    
+a = Solution()
+a.findTargetSumWays([1,1,1], 3)
+
+# 2D array DP solution
+
+
+class Solution:
+
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        dp = [[0 for i in range(3000)] for i in range(len(nums))]
+        
+        dp[0][1000+nums[0]] += 1
+        dp[0][1000-nums[0]] += 1
+        for i in range(1, len(nums)):
+            for j in range(2000):
+                dp[i][j] += dp[i-1][j + nums[i]] + dp[i-1][j - nums[i]] 
+        if 1000 + S <= 3000:
+            return dp[len(nums)-1][1000+S]
+        return 0
