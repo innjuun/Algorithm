94fb615ca9909475104f54f9ffdd26329488fcab
innjuun <sroo2315@gmail.com>
2020-12-04 01:46:49 +0900

    121 again

--- LeetCode/easy/121.py
+++ LeetCode/easy/121.py
@@ -8,3 +8,16 @@ class Solution:
             if prices[i] - min > max:
                 max = prices[i] - min
         return max
+    
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        minimum = int(1e9)
+        for i in range(len(prices)):
+            # print(maximum, minimum)
+            if prices[i] < minimum:
+                minimum = min(minimum, prices[i])   
+            elif prices[i] - minimum >= profit:
+                profit = max(profit, prices[i] - minimum)
+
+        return profit
