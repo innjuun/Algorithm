31df2ea501b9766fee184ed9f2731f5bdb26ecd5
innjuun <sroo2315@gmail.com>
2020-12-06 23:56:53 +0900

    53 solve again

--- LeetCode/easy/53.py
+++ LeetCode/easy/53.py
@@ -13,3 +13,15 @@ def maxSubArray(nums: List[int]) -> int:
     return max(memoi_list)
 
 print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [-int(2e9) for _ in range(len(nums))]
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+        
+        # print(dp)
+        return max(dp)
+            
+            
