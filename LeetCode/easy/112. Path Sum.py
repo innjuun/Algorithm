bb73b71de858e2d7976acfc6d3d11d41727ced84
innjuun <sroo2315@gmail.com>
2020-12-09 00:40:56 +0900

    112

--- LeetCode/easy/112. Path Sum.py
+++ LeetCode/easy/112. Path Sum.py
@@ -24,3 +24,27 @@ class Solution:
         travel(0, root)
         
         return self.flag
+    
+    
+# not using flag
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        def travel(total, node):
+            if not node:
+                return False
+            if node.left is None and node.right is None:
+                if total + node.val == sum:
+                    return True
+                return
+
+            return travel(total + node.val, node.left) or travel(total + node.val, node.right)
+        
+        return travel(0, root)
+        
