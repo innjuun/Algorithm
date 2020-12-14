8d3c45f22fb69efd64af567204f6cd6f9cb8e104
innjuun <sroo2315@gmail.com>
2020-12-14 22:51:23 +0900

    42 using stack

--- LeetCode/hard/42. Trapping Rain Water.py
+++ LeetCode/hard/42. Trapping Rain Water.py
@@ -1,20 +1,41 @@
+# class Solution:
+#     def trap(self, height: List[int]) -> int:
+        
+#         left_height = [0 for _ in range(len(height))]
+#         right_height = [0 for _ in range(len(height))]
+#         maximum = 0
+#         for i in range(len(height)):
+#             maximum = max(maximum, height[i])
+#             left_height[i] = max(maximum, height[i])
+#         maximum = 0
+#         for i in range(len(height)-1, -1, -1):
+#             maximum = max(maximum, height[i])
+#             right_height[i] = max(maximum, height[i])
+        
+#         print(left_height, right_height)
+#         answer = 0
+#         for i in range(len(height)):
+#             answer += min(left_height[i], right_height[i]) - height[i]
+#         return answer
+                
+# using stack       
 class Solution:
     def trap(self, height: List[int]) -> int:
+        volume = 0
+        stack = []
         
-        left_height = [0 for _ in range(len(height))]
-        right_height = [0 for _ in range(len(height))]
-        maximum = 0
         for i in range(len(height)):
-            maximum = max(maximum, height[i])
-            left_height[i] = max(maximum, height[i])
-        maximum = 0
-        for i in range(len(height)-1, -1, -1):
-            maximum = max(maximum, height[i])
-            right_height[i] = max(maximum, height[i])
+            # print(stack, volume)
+            
+            
+            while stack and height[i] >= height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                distance = i - stack[-1] - 1
+                water_depth = min(height[stack[-1]], height[i]) - height[top]
+                volume += distance * (water_depth)
+            
+            stack.append(i)
         
-        print(left_height, right_height)
-        answer = 0
-        for i in range(len(height)):
-            answer += min(left_height[i], right_height[i]) - height[i]
-        return answer
-                
+        return volume
