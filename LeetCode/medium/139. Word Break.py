e362c4226da76addd1a22e3c64750ccf52c95d26
innjuun <sroo2315@gmail.com>
2020-12-28 00:10:11 +0900

    139 memo

--- LeetCode/medium/139. Word Break.py
+++ LeetCode/medium/139. Word Break.py
@@ -25,3 +25,36 @@ class Solution:
         dfs(s, 0)
         
         return self.answer
+    
+
+# add memoization
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        self.answer = False
+        if not wordDict or not s:
+            return False
+        word_dict = set(wordDict)
+        self.cache = {}
+        if len(s) == 1:
+            if "".join(word_dict) == s:
+                return True
+            else:
+                return False
+
+        def dfs(s, index):
+            
+            if index == len(s):
+                self.answer = True
+                return
+            for i in range(index, len(s)):
+                if self.cache.get(i+1, False):
+                    continue
+                if s[index:i+1] in word_dict:
+                    self.cache[i+1] = True
+                    
+                    dfs(s, i+1)
+        dfs(s, 0)
+        # print(self.cache)
+        
+        return self.answer
