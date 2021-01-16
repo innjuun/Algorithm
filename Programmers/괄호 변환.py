49016d51a995a1211ced43c9cbb5c5e1dc49fbe2
innjuun <sroo2315@gmail.com>
2021-01-17 02:41:48 +0900

    괄호 변환

---
+++ Programmers/괄호 변환.py
@@ -0,0 +1,45 @@
+def solution(p):
+    if p == '':
+        return ''
+    if is_right(p):
+        return p
+
+    return dfs(p)
+
+
+def dfs(v):
+    for i in range(1, len(v) + 1):
+        if is_balanced(v[:i]):
+            concat = dfs(v[i:]) or ''
+
+            if is_right(v[:i]):
+                return v[:i] + concat
+            else:
+                return '(' + concat + ')' + make_reverse(v[1:i-1])
+
+
+def is_balanced(s):
+    return s.count('(') == s.count(')')
+
+
+def is_right(s):
+    stack = []
+    try:
+        for i in s:
+            if i == "(":
+                stack.append('foo')
+            else:
+                stack.pop()
+    except:
+        return False
+    return True
+
+def make_reverse(s):
+    res = []
+    for i in s:
+        if i == "(":
+            res.append(")")
+        else:
+            res.append("(")
+            
+    return ''.join(res)
