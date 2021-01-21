b97be5522daad8f88a42cee1e96d6733fa70f64d
innjuun <sroo2315@gmail.com>
2021-01-22 01:01:31 +0900

    정수 삼각형

--- Programmers/test/2.py
+++ Programmers/test/2.py
@@ -1,21 +1,4 @@
-def solution(answers):
-
-    a = [1,2,3,4,5] * 2005
-    b = [2,1,2,3,2,4,2,5] * 1300
-    c = [3,3,1,1,2,2,4,4,5,5] * 1010
-    dic = {'1': 0, '2': 0, '3': 0}
-    for i, ans in enumerate(answers):
-        if ans == a[i]:
-            dic['1'] = dic['1'] + 1
-        if ans == b[i]:
-            dic['2'] = dic['2'] + 1
-        if ans == c[i]:
-            dic['3'] = dic['3'] + 1
-    max_val = max(dic.values())
-    answer = []
-    for key, value in dic.items():
-        if value == max_val:
-            answer.append(key)
-    return sorted(answer)
-
-solution([1,3,2,4,2])
+def f(a, c=6, *b, **d):
+    print(a,b,c,d)
+    
+f(1,2,3,x=4)
---
+++ Programmers/정수 삼각형.py
@@ -0,0 +1,12 @@
+def solution(triangle):
+    dp = [[0 for j in range(i)] for i in range(1, 501)]
+    dp[0][0] = triangle[0][0]
+    for i in range(len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j] 
+    return max(dp[len(triangle)-1])
