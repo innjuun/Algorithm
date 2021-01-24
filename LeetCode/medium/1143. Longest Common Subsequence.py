3235a86070daa9ea5c2c4a1050e6c2baf15591c1
innjuun <sroo2315@gmail.com>
2021-01-24 17:24:58 +0900

    1143 with recursion, lru cache

--- LeetCode/medium/1143. Longest Common Subsequence.py
+++ LeetCode/medium/1143. Longest Common Subsequence.py
@@ -43,3 +43,14 @@ class Solution:
         return dp[len(text1)][len(text2)]
 
             
+import functools
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        @functools.lru_cache(maxsize=None)
+        def helper(i,j):
+            if i<0 or j<0:                
+                return 0
+            if text1[i]==text2[j]:
+                return helper(i-1, j-1) + 1
+            return max(helper(i-1, j), helper(i, j-1))
+        return helper(len(text1)-1,len(text2)-1)
