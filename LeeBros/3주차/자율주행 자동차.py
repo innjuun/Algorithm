2616aae52c576a3e4d596ee83a41984fe8b107d6
innjuun <sroo2315@gmail.com>
2021-02-22 01:53:26 +0900

    refactoring

--- LeeBros/3주차/자율주행 자동차.py
+++ LeeBros/3주차/자율주행 자동차.py
@@ -14,10 +14,6 @@ d = 10000 + d
 curr = [x, y]
 
 
-def is_in_grid(i, j):
-    return i >= 0 and j >= 0 and i < n and j < m
-
-
 def is_visited(i, j):
     return check[i][j]
 
@@ -27,7 +23,7 @@ def is_sidewalk(i, j):
 
 
 def is_can_go(i, j):
-    return is_in_grid(i, j) and not is_sidewalk(i, j)
+    return not is_visited(i, j) and not is_sidewalk(i, j)
 
 
 while True:
@@ -35,18 +31,19 @@ while True:
     check[curr[0]][curr[1]] = 1
     count = 0
     while count < 4:
-        print(curr, d)
+
         next_position = curr[0] + directions[((d - 1) % 4)][0], curr[1] + directions[(d - 1) % 4][1]
-        if not is_visited(next_position[0], next_position[1]) and is_can_go(next_position[0], next_position[1]):
+        if is_can_go(next_position[0], next_position[1]):
             d -= 1
             curr = next_position
             break
-        elif is_visited(next_position[0], next_position[1]) or not is_can_go(next_position[0], next_position[1]):
+        else:
             d -= 1
             count += 1
+    
     if count == 4:
         backward_position = curr[0] + directions[(d - 2) % 4][0], curr[1] + directions[(d - 2) % 4][1]
-        if is_can_go(backward_position[0], backward_position[1]):
+        if not is_sidewalk(backward_position[0], backward_position[1]):
             curr = [backward_position[0], backward_position[1]]
         else:
             break
